9447863fc594470cfc02391911f8c0f18f60c545
Alex Peterson <alexpeterson@Alexs-MacBook-Air-144.local>
2020-07-04 16:48:32 -0400

    remove method bodies for hashtable, linkedlist, and binarysearchtree

--- src/data-structures/hash-table/HashTable.js
+++ src/data-structures/hash-table/HashTable.js
@@ -35,13 +35,7 @@ export default class HashTable {
     //
     // where charCodeAt(i) is the i-th character code of the key, n is the length of the key and
     // PRIME is just any prime number like 31.
-    const hash = Array.from(key).reduce(
-      (hashAccumulator, keySymbol) => (hashAccumulator + keySymbol.charCodeAt(0)),
-      0,
-    );
-
-    // Reduce hash number so it would fit hash table size.
-    return hash % this.buckets.length;
+    
   }
 
   /**
@@ -49,18 +43,7 @@ export default class HashTable {
    * @param {*} value
    */
   set(key, value) {
-    const keyHash = this.hash(key);
-    this.keys[key] = keyHash;
-    const bucketLinkedList = this.buckets[keyHash];
-    const node = bucketLinkedList.find({ callback: nodeValue => nodeValue.key === key });
-
-    if (!node) {
-      // Insert new node.
-      bucketLinkedList.append({ key, value });
-    } else {
-      // Update value of existing node.
-      node.value.value = value;
-    }
+    
   }
 
   /**
@@ -68,16 +51,7 @@ export default class HashTable {
    * @return {*}
    */
   delete(key) {
-    const keyHash = this.hash(key);
-    delete this.keys[key];
-    const bucketLinkedList = this.buckets[keyHash];
-    const node = bucketLinkedList.find({ callback: nodeValue => nodeValue.key === key });
-
-    if (node) {
-      return bucketLinkedList.delete(node.value);
-    }
-
-    return null;
+  
   }
 
   /**
@@ -85,10 +59,7 @@ export default class HashTable {
    * @return {*}
    */
   get(key) {
-    const bucketLinkedList = this.buckets[this.hash(key)];
-    const node = bucketLinkedList.find({ callback: nodeValue => nodeValue.key === key });
 
-    return node ? node.value.value : undefined;
   }
 
   /**
@@ -96,13 +67,13 @@ export default class HashTable {
    * @return {boolean}
    */
   has(key) {
-    return Object.hasOwnProperty.call(this.keys, key);
+
   }
 
   /**
    * @return {string[]}
    */
   getKeys() {
-    return Object.keys(this.keys);
+
   }
 }
